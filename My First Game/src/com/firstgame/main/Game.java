6b01f97ebfaa11cd81081a1c3715aed8cb4e2d40
Severin <severin.weigold@edu.tbz.ch>
2018-11-09 16:59:37 +0100

    added some slow enemies

--- My First Game/src/com/firstgame/main/Game.java
+++ My First Game/src/com/firstgame/main/Game.java
@@ -41,6 +41,8 @@ public class Game extends Canvas implements Runnable {
 		handler.addObject(new Player(WIDTH/2-32,HEIGHT/2-32, ID.Player, handler));
 		handler.addObject(new Player2(WIDTH/2-50, HEIGHT/2-10, ID.Player2, handler));
 		handler.addObject(new SlowEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.SlowEnemy));
+		handler.addObject(new SlowEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.SlowEnemy));
+		handler.addObject(new SlowEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.SlowEnemy));
 		handler.addObject(new NormalEnemy(r.nextInt(WIDTH),r.nextInt(HEIGHT), ID.NormalEnemy));
 		handler.addObject(new FastEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.FastEnemy));
 		handler.addObject(new FastEnemy(r.nextInt(WIDTH), r.nextInt(HEIGHT), ID.FastEnemy));
